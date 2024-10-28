788d080e17b2e591a0608534c4dc61c25521767d
Juuso Karjanlahti <98909830+kuxacodes@users.noreply.github.com>
2024-10-28 17:00:53 +0200

    First commit

---
+++ src/main/java/fi/haagahelia/wiki_data_crud/WikiDataCrudApplication.java
@@ -0,0 +1,13 @@
+package fi.haagahelia.wiki_data_crud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WikiDataCrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WikiDataCrudApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fi/haagahelia/wiki_data_crud/WikiDataCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package fi.haagahelia.wiki_data_crud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WikiDataCrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
