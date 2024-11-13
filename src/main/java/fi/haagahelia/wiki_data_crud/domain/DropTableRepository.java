03c30f6d18f203af163036c83f8ce8e39a728241
Juuso Karjanlahti <98909830+kuxacodes@users.noreply.github.com>
2024-11-13 14:09:44 +0200

    Create repository interfaces for entities and delete old repository

--- src/main/java/fi/haagahelia/wiki_data_crud/domain/DropTableRepository.java
+++ src/main/java/fi/haagahelia/wiki_data_crud/domain/DropTableRepository.java
@@ -2,6 +2,6 @@ package fi.haagahelia.wiki_data_crud.domain;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface MonsterRepository extends JpaRepository<Monster, Long> {
-
+public interface DropTableRepository extends JpaRepository<DropTable, Long> {
+    
 }
