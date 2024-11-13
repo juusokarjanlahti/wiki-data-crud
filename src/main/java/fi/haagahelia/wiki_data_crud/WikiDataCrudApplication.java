2c22b507af633552ff9c4e2830e9101c3092176d
Juuso Karjanlahti <98909830+kuxacodes@users.noreply.github.com>
2024-11-13 14:12:48 +0200

    Create test data in application file

--- src/main/java/fi/haagahelia/wiki_data_crud/WikiDataCrudApplication.java
+++ src/main/java/fi/haagahelia/wiki_data_crud/WikiDataCrudApplication.java
@@ -4,41 +4,88 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
-import fi.haagahelia.wiki_data_crud.domain.DropItem;
-import fi.haagahelia.wiki_data_crud.domain.DropItemRepository;
 import fi.haagahelia.wiki_data_crud.domain.Monster;
 import fi.haagahelia.wiki_data_crud.domain.MonsterRepository;
+import jakarta.transaction.Transactional;
+import fi.haagahelia.wiki_data_crud.domain.DropTable;
+import fi.haagahelia.wiki_data_crud.domain.DropTableRepository;
+import fi.haagahelia.wiki_data_crud.domain.DropEntry;
+import fi.haagahelia.wiki_data_crud.domain.DropEntryRepository;
+import fi.haagahelia.wiki_data_crud.domain.Item;
+import fi.haagahelia.wiki_data_crud.domain.ItemRepository;
 
 @SpringBootApplication
 public class WikiDataCrudApplication {
-    private static final Logger log = LoggerFactory.getLogger(WikiDataCrudApplication.class);
 
     public static void main(String[] args) {
         SpringApplication.run(WikiDataCrudApplication.class, args);
     }
 
     @Bean
-    public CommandLineRunner demo(MonsterRepository monsterRepository, DropItemRepository dropTableRepository) {
+    @Transactional
+    public CommandLineRunner demo(MonsterRepository monsterRepository, DropTableRepository dropTableRepository, DropEntryRepository dropEntryRepository, ItemRepository itemRepository) {
         return (args) -> {
-            log.info("save a couple of monsters");
-            Monster chicken = new Monster("Chicken", "Bwak", 3);
-            Monster cow = new Monster("Cow", "Moo", 10);
-            monsterRepository.save(chicken);
-            monsterRepository.save(cow);
+            // Create items
+            Item item1 = new Item("Gold Coin", 100, true);
+            Item item2 = new Item("Dragon Scale", 1, false);
+            Item item3 = new Item("Health Potion", 1, false);
+            Item item4 = new Item("Silver Coin", 50, true);
+            Item item5 = new Item("Magic Scroll", 1, false);
+            Item item6 = new Item("Mana Potion", 1, false);
+            itemRepository.save(item1);
+            itemRepository.save(item2);
+            itemRepository.save(item3);
+            itemRepository.save(item4);
+            itemRepository.save(item5);
+            itemRepository.save(item6);
 
-            log.info("save a couple of drop tables");
-            DropItem chickenDropItem = new DropItem("Feather", 1, 0.5, chicken);
-            DropItem cowDropItem = new DropItem("Cowhide", 1, 0.5, cow);
-            dropTableRepository.save(chickenDropItem);
-            dropTableRepository.save(cowDropItem);
+            // Create first monster
+            Monster monster1 = new Monster("Dragon", "A fierce dragon", 100);
+            monsterRepository.save(monster1);
 
-            log.info("fetch all monsters");
-            for (Monster monster : monsterRepository.findAll()) {
-                log.info(monster.toString());
-            }
+            // Create drop table for first monster
+            DropTable dropTable1 = new DropTable("Dragon's Hoard");
+            dropTable1.setMonster(monster1);
+            dropTableRepository.save(dropTable1);
+
+            // Update monster with drop table
+            monster1.setDropTable(dropTable1);
+            monsterRepository.save(monster1);
+
+            // Create drop entries for first monster
+            DropEntry dropEntry1_1 = new DropEntry(dropTable1, item1, 100, 0.5);
+            DropEntry dropEntry1_2 = new DropEntry(dropTable1, item2, 1, 0.1);
+            DropEntry dropEntry1_3 = new DropEntry(dropTable1, item3, 1, 0.2);
+            dropEntryRepository.save(dropEntry1_1);
+            dropEntryRepository.save(dropEntry1_2);
+            dropEntryRepository.save(dropEntry1_3);
+
+
+            dropTableRepository.save(dropTable1);
+
+            // Create second monster
+            Monster monster2 = new Monster("Goblin", "A sneaky goblin", 20);
+            monsterRepository.save(monster2);
+
+            // Create drop table for second monster
+            DropTable dropTable2 = new DropTable("Goblin's Loot");
+            dropTable2.setMonster(monster2);
+            dropTableRepository.save(dropTable2);
+
+            // Update monster with drop table
+            monster2.setDropTable(dropTable2);
+            monsterRepository.save(monster2);
+
+            // Create drop entries for second monster
+            DropEntry dropEntry2_1 = new DropEntry(dropTable2, item4, 50, 0.6);
+            DropEntry dropEntry2_2 = new DropEntry(dropTable2, item5, 1, 0.05);
+            DropEntry dropEntry2_3 = new DropEntry(dropTable2, item6, 1, 0.3);
+            dropEntryRepository.save(dropEntry2_1);
+            dropEntryRepository.save(dropEntry2_2);
+            dropEntryRepository.save(dropEntry2_3);
+
+            dropTableRepository.save(dropTable2);
         };
     }
 }
