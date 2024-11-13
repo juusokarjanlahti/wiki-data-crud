1a985c9cbc8d8538cb713adec3e0d3cd37ec5fee
Juuso Karjanlahti <98909830+kuxacodes@users.noreply.github.com>
2024-11-13 15:08:07 +0200

    Delete old repositories

--- src/main/java/fi/haagahelia/wiki_data_crud/repository/DropTableRepository.java
+++
@@ -1,9 +0,0 @@
-package fi.haagahelia.wiki_data_crud.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import fi.haagahelia.wiki_data_crud.domain.DropTable;
-
-public interface DropTableRepository extends JpaRepository<DropTable, Long> {
-    
-}
